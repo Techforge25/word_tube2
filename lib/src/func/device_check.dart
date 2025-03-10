cafa4414fe136a16ce9d91b417ebf8ce3c5b12e6
AliSyedTech <syed.ali.mehdi@techigator.com>
2025-03-10 21:38:33 +0500

    re-initializing speach to text

--- lib/src/func/device_check.dart
+++ lib/src/func/device_check.dart
@@ -3,11 +3,9 @@ import 'package:device_info_plus/device_info_plus.dart';
 class DeviceCheck {
   static Future<bool> isMobile() async {
     final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-
     IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
 
     if (iosInfo.model.toLowerCase().contains('ipad')) return false;
-
     return true;
   }
 }
